071a1aca9464525bcc168c87478473f6295d150a
Ben Border <ben@bunnbuns.net>
2019-11-07 13:02:39 -0500

    .com fix

--- mc-banner.js
+++ mc-banner.js
@@ -11,7 +11,7 @@ getMC.onreadystatechange = function() {
             var tmp = JSON.parse(getMC.responseText);
             if(tmp.online == false){
                 console.log(tmp);
-                document.getElementById('mc-banner').innerHTML = '<div class="mc-top"> <img src="server-icons/mc.stibarc.gq.png"> <span class="mc-sub-top"> <span class="mc-title">mc.stibarc.gq</span> <span class="mc-header" style="color:#AA0000;">Can\'t connect to server</span> </span> </div> <h4 id="playercount"> <div style="display:inline;margin: 0 5px 0 0;"> <span style="color:#AAAAAA;"></span> <span style="color:#555555;"></span> <span style="color:#AAAAAA;"></span> </div> <span class="couponcode"> <div class="ping-badge bad"> </div></span> </h4> <div style="margin:0 0 5px 0;"> <h4 style="margin:1rem 0">Players Online:</h4> <span id="playerlist">None</span> </div> <br /> <div id="refresh" class="mc-button" style="image-rendering: pixelated;" onclick="loadMCBanner()"> <span style="font-family: Minecraft, sans-serif;">Refresh</span> </div>'; 
+                document.getElementById('mc-banner').innerHTML = '<div class="mc-top"> <img src="server-icons/mc.stibarc.com.png"> <span class="mc-sub-top"> <span class="mc-title">mc.stibarc.com</span> <span class="mc-header" style="color:#AA0000;">Can\'t connect to server</span> </span> </div> <h4 id="playercount"> <div style="display:inline;margin: 0 5px 0 0;"> <span style="color:#AAAAAA;"></span> <span style="color:#555555;"></span> <span style="color:#AAAAAA;"></span> </div> <span class="couponcode"> <div class="ping-badge bad"> </div></span> </h4> <div style="margin:0 0 5px 0;"> <h4 style="margin:1rem 0">Players Online:</h4> <span id="playerlist">None</span> </div> <br /> <div id="refresh" class="mc-button" style="image-rendering: pixelated;" onclick="loadMCBanner()"> <span style="font-family: Minecraft, sans-serif;">Refresh</span> </div>'; 
             }else{
                 console.log(tmp);
                 document.getElementById('mc-banner').innerHTML = '<div class="mc-top"> <img src="'+tmp.icon+'"> <span class="mc-sub-top"> <span class="mc-title">'+tmp.hostname+'</span> <span class="mc-header">'+tmp.motd.html+'</span> </span> </div> <h4 id="playercount"> <div style="display:inline;margin: 0 5px 0 0;"> <span style="color:#AAAAAA;">'+tmp.players.online+'</span> <span style="color:#555555;">/</span> <span style="color:#AAAAAA;">'+tmp.players.max+'</span> </div> <span class="couponcode"> <div class="ping-badge great"> </div></span> </h4> <div style="margin:0 0 5px 0;"> <h4 style="margin:1rem 0">Players Online:</h4> <span id="playerlist">Loading...</span> </div> <br /> <div id="refresh" class="mc-button" style="image-rendering: pixelated;" onclick="loadMCBanner()"> <span style="font-family: Minecraft, sans-serif;">Refresh</span> </div>';
@@ -23,7 +23,7 @@ getMC.onreadystatechange = function() {
 // --- Functions --- //
 function loadMCBanner(){ //send request and kickstart the rest of the banner
     document.getElementById('mc-banner').innerHTML = '<center><div class="lds-ellipsis white"><div></div><div></div><div></div><div></div></div></center>';
-    getMC.open("GET", "https://api.mcsrvstat.us/2/mc.stibarc.gq", true);
+    getMC.open("GET", "https://api.mcsrvstat.us/2/mc.stibarc.com", true);
     getMC.send();
 }
 function getPlayerList(tmp){ //use the json requested to list all players
